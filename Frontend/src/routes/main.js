ab5046c7371433c3d5f4a8fa96d7ae4dfa1d2bc5
Evo123456 <34413640+Evo123456@users.noreply.github.com>
2019-03-05 21:18:48 -0500

    Login Page Added

--- Frontend/src/routes/main.js
+++ Frontend/src/routes/main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Login } from '../modules/Login';
 import { Home } from '../modules/Home';
-
 import { Switch, Route } from 'react-router-dom';
+import withStyles from './../modules/Login';
+
 
 const Main = () => (
   <Switch>
-    <Route exact path='/Login' component={Login} />
-    <Route exact path='/' component={Home} />
+    <Route exact path='/' component={withStyles} />
   </Switch>
 )
 
