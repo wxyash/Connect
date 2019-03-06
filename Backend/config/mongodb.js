33f9c883bcda739604f9e8628a4edd37ca398f0e
Yash Thanki <yash@unoapp.com>
2019-03-05 19:13:17 -0500

    socket

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,8 +1,8 @@
 const CONFIG = require('./config');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://'+CONFIG.db_host+'/'+CONFIG.db_name, { useNewUrlParser: true }).then((connection) => {
+mongoose.connect(`mongodb://${CONFIG.db_user}:${CONFIG.db_password}@${CONFIG.db_host}:${CONFIG.db_port}/${CONFIG.db_name}`, { useNewUrlParser: true }).then((connection) => {
     console.log('connected successfully');
 }).catch((err) => {
-    console.log('error connecting');
+    console.log(err.message);
 });
 module.exports = mongoose
