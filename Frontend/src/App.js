867f06c3ea3cc2ad562862d777ee3e8e74779f37
Yash Thanki <yash@unoapp.com>
2019-04-17 20:53:40 -0400

    MEssage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -23,15 +23,21 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  button: {
+    color: 'white',
+    border: '1px solid white'
+  }
 };
 
 class MenuAppBar extends React.Component {
   state = {
     auth: true,
     anchorEl: null,
-    userLoggedIn: this.CheckLogin()
+    userLoggedIn: ''
   };
-
+  componentDidMount () {
+    this.setState({userLoggedIn: this.CheckLogin()})
+  }
   handleChange = event => {
     this.setState({ auth: event.target.checked });
   };
@@ -49,27 +55,55 @@ class MenuAppBar extends React.Component {
     console.log("I am here")
   }
 
-CheckLogin(params) {
+CheckLogin = (params) => {
     let token = localStorage.getItem('token')
+    // if (!token) {
+    //   return <div>
+    //     <MenuItem onClick={this.handleClose} component={Link} to='/AdminRegister'>Admin Register</MenuItem>
+    //     <MenuItem onClick={this.handleClose} component={Link} to='/AdminLogin'>Admin Login</MenuItem>
+    //     <MenuItem onClick={this.handleClose} component={Link} to='/Register'>User Register</MenuItem>
+    //     <MenuItem onClick={this.handleClose} component={Link} to='/'>User Login</MenuItem>
+    //   </div>
+    // } else {
+    //   return <div>
+    //       <MenuItem onClick={() => {
+    //         this.handleClose()
+    //         localStorage.clear()
+    //       }} component={Link} to='/AdminLogin'> Logout </MenuItem>
+    //     </div>
+    // }
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     if (!token) {
-      return <div>
-        <MenuItem onClick={this.handleClose} component={Link} to='/AdminRegister'>Admin Register</MenuItem>
-        <MenuItem onClick={this.handleClose} component={Link} to='/AdminLogin'>Admin Login</MenuItem>
-        <MenuItem onClick={this.handleClose} component={Link} to='/Register'>User Register</MenuItem>
-        <MenuItem onClick={this.handleClose} component={Link} to='/'>User Login</MenuItem>
-      </div>
-    } else {
-      return <div>
-          <MenuItem onClick={this.handleClose}> Logout </MenuItem>
+      return (
+        <div>
+      <Button variant="outlined" component={Link} to='/AdminRegister' className={classes.button}>
+      Admin Register
+      </Button>
+      <Button component={Link} to='/AdminLogin' variant="outlined" className={classes.button}>
+      Admin Login
+      </Button>
+      <Button component={Link} to='/Register' variant="outlined" className={classes.button}>
+      User Register
+      </Button>
+      <Button component={Link} to='/' variant="outlined" className={classes.button}>
+      User Login
+      </Button>
         </div>
+      )
+    } else {
+          return ( <Button onClick={() => {
+            localStorage.clear()
+          }} variant="contained" color="secondary" className={classes.button}>
+      Logout
+    </Button>)
     }
   }
 
   render() {
     const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
-
+    const { auth } = this.state;
     return (
       <div className={classes.root}>
         <AppBar position="static">
@@ -82,30 +116,7 @@ CheckLogin(params) {
             </Typography>
             {auth && (
               <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
                 {this.state.userLoggedIn}
-                </Menu>
               </div>
             )}
           </Toolbar>
